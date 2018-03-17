e2012737455a9ac63030524fbccce4ce09763c05
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-03-17 15:04:39 -0700

    c.2.1

--- workspace/IntroToExpress/FirstExpressApp/app.js
+++ workspace/IntroToExpress/FirstExpressApp/app.js
@@ -11,10 +11,39 @@ app.get("/", function(req, res) {
 app.get("/bye", function(req, res){
     res.send("Goodbye!!");
 });
+
+// // "/r/subredditName" => "Goodbye!"
+// app.get("/r/:subredditName", function(req, res){
+//     console.log(req.params);
+//     res.send("Wellcome To a Subreddit!");
+// });
+
+// "/r/subredditName" => "Goodbye!"
+app.get("/r/:subredditName", function(req, res){
+    var subreddit = req.params.subredditName;
+    res.send("Wellcome To The " + subreddit.toUpperCase() + "SUBREDDIT!");
+});
+
+// // "/r/subredditName/comments/:id/:title/" => "Goodbye!"
+// app.get("/r/:subredditName/comments/:id/:title/", function(req, res){
+//     console.log(req.params);
+//     res.send("Wellcome To a Comments Page!");
+// });
+
+// "/r/subredditName/comments/:id/:title/" => "Goodbye!"
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res){
+    console.log(req.params);
+    res.send("Wellcome To a Comments Page!");
+});
+
 // "/dog" => "Meow!"
 app.get("/dog", function(req, res){
    res.send("MEOW!");
    console.log("some made a request to DOG!!");
+});
+
+app.get("*", function(req, res){
+    res.send("You are a star.");
 });
 
 // Tell Express to listen for requests (start server)
